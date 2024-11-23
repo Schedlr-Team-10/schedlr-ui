ca71f825e94e2bce3f3b4c63f61e0c7e227ed5e0
Ravi Shankar <Bhangeravishankar2302@gmail.com>
2024-11-23 15:54:07 -0500

    removed facebook option

--- src/Components/Createpost.js
+++ src/Components/Createpost.js
@@ -170,7 +170,7 @@ const CreatePost = () => {
             </button>
 
             <div className="grid grid-cols-2 gap-4">
-              {["LinkedIn", "PInterest", "Twitter", "Facebook"].map(
+              {["LinkedIn", "PInterest", "Twitter"].map(
                 (platform) => (
                   <button
                     key={platform}
