5f9990f377045bac4e8159f26c8614edef3f211b
Ravi Shankar <Bhangeravishankar2302@gmail.com>
2024-11-23 17:10:40 -0500

    removed colorfull background

--- src/Components/Createpost.js
+++ src/Components/Createpost.js
@@ -118,7 +118,7 @@ const CreatePost = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-yellow-400 via-pink-400 to-purple-500 flex flex-col items-center justify-center p-8 text-gray-800">
+    <div className="min-h-screen bg-gradient-to-br flex flex-col items-center justify-center p-8 text-gray-800 ">
       {/* Main Container */}
       <div className="max-w-4xl w-full bg-white rounded-2xl shadow-xl p-8">
         <h1 className="text-4xl font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-500 mb-8">
@@ -169,7 +169,7 @@ const CreatePost = () => {
               Generate Description
             </button>
 
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-3 gap-3">
               {["LinkedIn", "PInterest", "Twitter"].map(
                 (platform) => (
                   <button
